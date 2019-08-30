ba329bcb1b71c92fd54e10ac88c8f0d0b1ee0965
Nick <nst447@gmail.com>
2019-08-30 16:11:40 -0700

    Adding brackets to database_url. Trying to fix search bar bug.

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ app = Flask(__name__)
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgres:///warbler'))
+    os.environ.get('[DATABASE_URL]', 'postgres:///warbler'))
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "nevertell")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
